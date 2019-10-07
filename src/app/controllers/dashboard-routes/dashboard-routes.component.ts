10ae5b700edc9eb538e93d31c5d739c0e29ef4aa
Anuj Varshney <anuj.varshney@rategain.com>
2019-10-07 23:19:07 +0530

    Right Slider Final Fixes

--- src/app/controllers/dashboard-routes/dashboard-routes.component.ts
+++ src/app/controllers/dashboard-routes/dashboard-routes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -10,8 +10,8 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class DashboardRoutesComponent implements OnInit {
   options: FormGroup;
- 
- 
+  showIconsOnly: boolean = true;
+
   constructor(private router: Router, fb: FormBuilder) {
     this.options = fb.group({
       bottom: 0,
@@ -26,5 +26,18 @@ export class DashboardRoutesComponent implements OnInit {
     this.router.navigate(['']);
     localStorage.removeItem('usermanagementToken');
   }
- 
+
+  toggleRightNav(): void {
+    if (this.showIconsOnly) {
+      this.showIconsOnly = false;
+    } else {
+      this.showIconsOnly = true;
+    }
+    document.getElementById('rtBttn').click();
+    setTimeout(function () {
+      document.getElementById('rtBttn').click();
+    }, 1);
+
+  }
+
 }
