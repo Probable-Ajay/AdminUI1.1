47998f282a9811edc4a07d7a4bb26668939ae121
probable-ajay <probable.ajay@gmail.com>
2019-10-11 00:14:12 +0530

    [login changes]

--- src/app/controllers/user-login/user-login.component.ts
+++ src/app/controllers/user-login/user-login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
 import { LoginService } from "../../services/login.service";
 import { userParam } from "../../models/user";
 
@@ -12,18 +14,42 @@ export class UserLoginComponent implements OnInit {
   userObject: userParam = new userParam();
   _encrypteduser: string;
   _encryptedpassword: string;
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor(private router: Router, private LoginService: LoginService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private LoginService: LoginService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
 
-  userLogin(): void {
-    if (!this.userObject.email || !this.userObject.password) {
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    if (!this.f.username.value || !this.f.password.value) {
       alert("Please enter proper details...");
     } else {
       if (
-        this.userObject.email == "admin" &&
-        this.userObject.password == "admin"
+        this.f.username.value == "admin" &&
+        this.f.password.value == "admin"
       ) {
         this.router.navigate(["dashboard"]);
       } else {
