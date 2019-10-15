8201f3c5351cf11d15bbd7eb0fd8a3013a508006
probable-ajay <probable.ajay@gmail.com>
2019-10-15 22:52:49 +0530

    [merge changes]

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { BehaviorSubject, Observable } from "rxjs";
 import { map } from "rxjs/operators";
+import { Router, ROUTES } from "@angular/router";
 import { AppSettings } from "../_configurations/AppSettings";
-import { Router, ROUTES } from '@angular/router';
 import { User, userRegistration, Login, RegisterDemo } from "../_models";
 
 @Injectable({
@@ -23,70 +23,59 @@ export class AuthenticationService {
     return this.currentUserSubject.value;
   }
 
-  // login(username: string, password: string) {
-  //   return this.http
-  //     .post<any>(`/users/authenticate`, { username, password })
-  //     .pipe(
-  //       map(user => {
-  //         // login successful if there's a jwt token in the response
-  //         if (user && user.token) {
-  //           // store user details and jwt token in local storage to keep user logged in between page refreshes
-  //           localStorage.setItem("currentUser", JSON.stringify(user));
-  //           this.currentUserSubject.next(user);
-  //         }
-
-  //         return user;
-  //       })
-  //     );
-  // }
-
-  // register(user: userRegistration) {
-  //   return this.http.post<any>(`/users/create`, { user }).pipe(
-  //     map(user => {
-  //       // login successful if there's a jwt token in the response
-  //       if (user && user.token) {
-  //         // store user details and jwt token in local storage to keep user logged in between page refreshes
-  //         localStorage.setItem("currentUser", JSON.stringify(user));
-  //         this.currentUserSubject.next(user);
-  //       }
-
-  //       return user;
-  //     })
-  //   );
-  // }
-
-  login(params: Login) {
+  login(user: userRegistration) {
     const apiUrl = AppSettings.API_ENDPOINT + "login";
     const headers = new HttpHeaders().set("Content-Type", "application/json");
-
-    return this.http.post(apiUrl, params, { headers }).pipe(map(data => {
-      debugger;
-      if (data['status'] == 'success') {
-        localStorage.setItem("currentUser", data['data']['token']);
-        this.router.navigate(["/dashboard/"]);
-      }
-      else {
-        localStorage.removeItem("currentUser");
-        this.router.navigate(["/login"]);
-      }
-    }));
+    return this.http.post<any>(apiUrl, user, { headers }).pipe(
+      map(user => {
+        // login successful if there's a jwt token in the response
+        if (user && user["data"]["token"]) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem(
+            "currentUser",
+            JSON.stringify(user["data"]["token"])
+          );
+          this.currentUserSubject.next(user);
+          debugger;
+          this.router.navigate(["/dashboard/"]);
+        } else {
+          localStorage.removeItem("currentUser");
+          this.router.navigate(["/login"]);
+        }
+      })
+    );
   }
 
   register(user: userRegistration) {
     const apiUrl = AppSettings.API_ENDPOINT + "users/create";
     const headers = new HttpHeaders().set("Content-Type", "application/json");
-    return this.http.post(apiUrl, user, { headers });
+    return this.http.post<any>(apiUrl, { user }, { headers }).pipe(
+      map(user => {
+        // login successful if there's a jwt token in the response
+        if (user) {
+          // // store user details and jwt token in local storage to keep user logged in between page refreshes
+          // localStorage.setItem("auth-token", JSON.stringify(user));
+          // this.currentUserSubject.next(user);
+        }
 
-    //return this.httpClient.post(`/users/register`, user);
+        return user;
+      })
+    );
   }
 
-  requestDemo(params: RegisterDemo) {
-    debugger;
-    const apiUrl = AppSettings.API_ENDPOINT + "demo/create";
-    const headers = new HttpHeaders().set("Content-Type", "application/json");
+  // register(user: userRegistration) {
+  //   const apiUrl = AppSettings.API_ENDPOINT + "users/create";
+  //   const headers = new HttpHeaders().set("Content-Type", "application/json");
+  //   return this.http.post(apiUrl, user, { headers });
+  // }
 
-    return this.http.post(apiUrl, params, { headers });
-  }
+  // requestDemo(params: RegisterDemo) {
+  //   debugger;
+  //   const apiUrl = AppSettings.API_ENDPOINT + "demo/create";
+  //   const headers = new HttpHeaders().set("Content-Type", "application/json");
+
+  //   return this.http.post(apiUrl, params, { headers });
+  // }
 
   logout() {
     // remove user from local storage to log user out
